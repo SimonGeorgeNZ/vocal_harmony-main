ec0130fcfdec229b4ae8ee75cf6668a10ead0d67
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2023-02-23 21:12:58 +0000

    Splitting functions

--- app.py
+++ app.py
@@ -3,33 +3,52 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
+
 if path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'vocal_harmony'
+app.config["MONGO_DBNAME"] = "vocal_harmony"
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
-MONGO_URI = os.environ.get('MONGO_URI')
-MONGO_DBNAME = os.environ.get('MONGO_DBNAME') 
+MONGO_URI = os.environ.get("MONGO_URI")
+MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
+
+db = mongo.db.keys
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    sel_key = []
-    noteString = ""
+def get_data():
     key_Sig = list(mongo.db.keys.find())
-    if request.method == 'POST':
-        selected_key = request.form.get('keySig')
-        sel_key = mongo.db.keys.find_one({'keySig': selected_key})
-        keyNotes = sel_key['notes']
-        noteString = ', '.join(keyNotes)
-    return render_template('index.html', ks=key_Sig, sk=sel_key, notes=noteString)
+    return key_Sig
 
 
-if __name__ == '__main__':
-    app.run(debug=False)
+def pick_your_key():
+    sel_key = []
+    if request.method == "POST":
+        selected_key = request.form.get("keySig")
+        sel_key = mongo.db.keys.find_one({"keySig": selected_key})
+        return sel_key
 
+
+def get_scale_notes():
+    noteString = ""
+    sel_key = [pick_your_key()]
+    keyNotes = [sel_key]
+    # noteString = ", ".join(keyNotes)
+    return noteString
+
+
+@app.route("/", methods=["GET", "POST"])
+def Load_the_page():
+    sel_key = pick_your_key()
+    key_Sig = get_data()
+    pick_your_key()
+    noteString = get_scale_notes()
+    return render_template("index.html", ks=key_Sig, sk=sel_key, notes=noteString)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
