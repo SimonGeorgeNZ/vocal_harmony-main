1fb5831eccb2f086d9bfb9cd40b70ac1f09636a2
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2023-02-28 09:43:06 +0000

    New requirements file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from playsound import playsound
 from os import path
 
 if path.exists("env.py"):
@@ -40,7 +41,7 @@ def get_scale_notes():
 
 
 @app.route("/", methods=["GET", "POST"])
-def Load_the_page():
+def load_the_page():
     return render_template(
         "index.html",
         ks=get_data(),
