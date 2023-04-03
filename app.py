cde7152d1e3904d2236aa958171ebdb76955c284
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2023-04-03 22:47:59 +0100

    Selected harmony notes returning

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from playsound import playsound
@@ -32,36 +32,48 @@ def get_data():
 # Used to display specific key note in "Your key is"#
 
 
+def set_key(key):
+    info = all_info(key)
+    key = info["keySig"]
+    return key
+
+
 @app.route("/", methods=["GET", "POST"])
 def load_the_page():
     return render_template(
         "index.html",
         ks=get_data(),
-        # sk=pick_your_key(),
         # notes=get_scale_notes(),
     )
 
 
-# def set_key(key):
-#     picked = mongo.db.keys.find_one({"keySig": key})
-#     key = picked["keySig"]
-#     return key
+# Gets all info from DB needs the (key) attributed to work
+
+
+def all_info(key):
+    # allinfo = mongo.db.keys.find_one({"keySig": request.form.get("keySig")})
+    all_info = mongo.db.keys.find_one({"keySig": key})
+    return all_info
 
 
 @app.route("/harmony/<key>", methods=["POST", "GET"])
 def harmony(key):
-    picked = mongo.db.keys.find_one({"keySig": key})
-    result = file_path()
-    Keynotes = picked["notes"]
-    key = picked["keySig"]
+    # path = file_path()
+    key = set_key(key)
+    get_notes = all_info(key)
+    Keynotes = get_notes["notes"]
     return render_template(
         "index.html",
-        key=key,
+        sk=set_key(key),
         Keynotes=Keynotes,
+        key=key,
         ks=get_data(),
         root=get_root(),
-        result=result,
+        # path=path,
     )
+
+
+# Gets note to harmonise against#
 
 
 def get_root():
@@ -70,14 +82,14 @@ def get_root():
         return root
 
 
-def file_path():
-    if request.method == "POST":
-        root = get_root()
-        first = "./media/"
-        second = ".wav"
-        result = first + root + second
-        playsound(result)
-        return result
+# def file_path():
+#     if request.method == "POST":
+#         root = get_root()
+#         first = "./media/"
+#         second = ".wav"
+#         result = first + root + second
+#         playsound(result)
+#         return result
 
 
 # @app.route("/tone/<key>", methods=["POST", "GET"])
@@ -99,26 +111,62 @@ def get_harm(key):
     picked = mongo.db.keys.find_one({"keySig": key})
     key = picked["keySig"]
     Keynotes = picked["notes"]
-    path = file_path()
-    compare = start + {{note}} + end
-    start = "./media/"
-    end = ".wav"
-    print(compare)
+    # path = file_path()
+    # print(path)
+    # note = "A"
+    # compare = start + note + end
+    # start = "./media/"
+    # end = ".wav"
     return Keynotes
 
 
 @app.route("/harmony_notes/<key>", methods=["POST", "GET"])
 def get_harm_notes(key):
-    picked = mongo.db.keys.find_one({"keySig": key})
-    Keynotes = picked["notes"]
-    key = picked["keySig"]
+    key = set_key(key)
+    get_notes = all_info(key)
+    Keynotes = get_notes["notes"]
+    string = "harmSelect"
     if request.method == "POST":
-        return render_template(
-            "index.html",
-            ks=get_data(),
-            root=get_root(),
-            Keynotes=Keynotes,
-        )
+        for x in Keynotes:
+            inputname = string + x
+            formname = request.form.get(inputname)
+            print(formname)
+    return render_template(
+        "index.html",
+        ks=get_data(),
+        Keynotes=Keynotes,
+        key=key,
+    )
+
+
+# @app.route("/harmony/<key>", methods=["POST", "GET"])
+# def try_get(key):
+#     picked = mongo.db.keys.find_one({"keySig": key})
+#     Keynotes = picked["notes"]
+#     key = picked["keySig"]
+#     for a in Keynotes:
+#         note = a
+#         name = "harmSelect"
+#         string = name + note
+#     if request.method == "POST":
+#         Keynotes = picked["notes"]
+#         playedNotes = request.form.get("string")
+#         print(playedNotes)
+
+#     print(playedNotes)
+
+#     for x in playedNotes:
+#         print(x)
+#     for a in playedNotes:
+#         print(a)
+#         print(x)
+#         print(compare)
+#     return render_template(
+#         "index.html",
+#         Keynotes=Keynotes,
+#         ks=get_data(),
+#         key=key,
+#     )
 
 
 if __name__ == "__main__":
